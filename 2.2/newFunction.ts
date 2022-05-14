3a8f3629c353982c4c2458f44002927def5bcf03
unknown <nehajha.1987@gmail.com>
2022-05-14 10:13:54 -0700

    adding homework11May

--- 2.2/newFunction.ts
+++ 2.2/newFunction.ts
@@ -1,1 +1,15 @@
-export function myFunction() {}
+export function myFunction(myNum: number): string {
+    if (myNum == 5) {
+        return 'true'
+    } else if (myNum < 5 && myNum >= 0) {
+        return 'false'
+    } else if (myNum > 5) {
+        return 'big'
+    } else if (myNum < 0) {
+        return 'negative'
+    }
+}
+console.log(myFunction(-2));
+console.log(myFunction(50));
+console.log(myFunction(4));
+console.log(myFunction(5))
